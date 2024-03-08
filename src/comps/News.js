6f3a252295cd6cf95a1488e24a5c5b75487f0f78
OSova2023 <ovf2019@gmail.com>
2024-03-08 14:12:52 +0300

    cards_css

--- src/comps/News.js
+++ src/comps/News.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import "./news.css";
 
 const News = () => {
   const API = "bea58f088a294fe1a4a0f086e912d823";
@@ -25,23 +26,34 @@ const News = () => {
   }, []);
 
   return (
-    <div>
+    <div className="mainDiv">
       {myNews.map((element) => {
         console.log(element);
 
         return (
-          <div className="mainDiv">
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={element.urlToImage} />
-              <Card.Body>
-                <Card.Title>{element.title}</Card.Title>
-                <Card.Text>{element.title}</Card.Text>
-                <Button variant="primary" href={element.url} target="_blank">
-                  Go to the source
-                </Button>
-              </Card.Body>
-            </Card>
-          </div>
+          <Card className="card" style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              src={
+                element.urlToImage == null
+                  ? "https://www.newstyle-mag.com/wp-content/uploads/2017/02/232-286x300.jpg"
+                  : element.urlToImage
+              }
+              style={{ maxHeight: "10rem", objectFit: "cover" }}
+            />
+            <Card.Body>
+              <Card.Title>
+                {element.source.name == ""
+                  ? element.author
+                  : element.source.name}
+              </Card.Title>
+              <Card.Text>{element.title}</Card.Text>
+              <Card.Text className="date">{element.publishedAt}</Card.Text>
+              <Button variant="primary" href={element.url} target="_blank">
+                Go to the source
+              </Button>
+            </Card.Body>
+          </Card>
         );
       })}
     </div>
